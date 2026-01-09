2150cc2c2d1815d42e310a724338329343fc1b03
regisx001 <zarqi.ezzoubair@etu.uae.ac.ma>
2026-01-09 18:09:03 +0100

    Implement Tree and TreeEntry classes with serialization and hashing methods for better structure and data management

--- src/main/java/com/regisx001/minigit/domain/Tree.java
+++ src/main/java/com/regisx001/minigit/domain/Tree.java
@@ -1,5 +1,28 @@
 package com.regisx001.minigit.domain;
 
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import com.regisx001.minigit.utils.HashUtil;
+
 public class Tree {
-    
+
+    private final List<TreeEntry> entries;
+
+    public Tree(List<TreeEntry> entries) {
+        this.entries = entries;
+    }
+
+    public byte[] serialize() {
+        String body = entries.stream()
+                .map(TreeEntry::serialize)
+                .reduce("", (a, b) -> a + b + "\n");
+
+        String header = "tree " + body.length() + "\0";
+        return (header + body).getBytes(StandardCharsets.UTF_8);
+    }
+
+    public String hash() {
+        return HashUtil.sha1(serialize());
+    }
 }
--- src/main/java/com/regisx001/minigit/domain/TreeEntry.java
+++ src/main/java/com/regisx001/minigit/domain/TreeEntry.java
@@ -1,5 +1,18 @@
 package com.regisx001.minigit.domain;
 
 public class TreeEntry {
-    
+
+    private final String mode;
+    private final String name;
+    private final String hash;
+
+    public TreeEntry(String mode, String name, String hash) {
+        this.mode = mode;
+        this.name = name;
+        this.hash = hash;
+    }
+
+    public String serialize() {
+        return mode + " " + name + " " + hash;
+    }
 }
